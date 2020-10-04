16e7f30915099c6aa1ee516103610bf16e025413
SR NUVEM <devnanuvem@protonmail.com>
2020-10-04 18:08:36 -0300

    [NUVEM-0001] fixing code review comments

--- src/app/services/campaign.service.ts
+++ src/app/services/campaign.service.ts
@@ -482,9 +482,9 @@ export class CampaignService {
     self.methods.anyKnownNPCs = (): boolean => self.methods.listKnownNPCs().length > 0
     self.methods.isActiveNPC = (npc: CampaignNpc): boolean => npc.id === self.locals.active_npc
 
-    self.methods.addNPC = (npc: CampaignNpc = new CampaignNpc, name: string): void => {
+    self.methods.addNPC = (npc: CampaignNpc = new CampaignNpc): void => {
       npc.pos = self.methods.listNPCs().length
-      npc.name = name ? name : `${this.sheetSvc.randomName()} ${this.sheetSvc.randomName()}`
+      npc.name = npc.name ? npc.name : `${this.sheetSvc.randomName()} ${this.sheetSvc.randomName()}`
       self.methods.$add(self.model, 'npcs', CampaignNpc, npc)
       self.locals.active_npc = self.model.npcs[self.model.npcs.length - 1].id
     }
@@ -526,28 +526,30 @@ export class CampaignService {
     self.methods.listKnownFoes = (): CampaignFoe[] => self.methods.listFoes().filter(x => x.known === true)
     self.methods.anyKnownFoes = (): boolean => self.methods.listKnownFoes().length > 0
 
-    self.methods.addFoe = (foe: CampaignFoe = new CampaignFoe, name: string): void => {
+    self.methods.addFoe = (foe: CampaignFoe = new CampaignFoe): void => {
       foe.pos = self.methods.listFoes().length
-      foe.name = name ? name : `${this.sheetSvc.randomName()} ${this.sheetSvc.randomName()}`
+      foe.name = foe.name ? foe.name : `${this.sheetSvc.randomName()} ${this.sheetSvc.randomName()}`
       self.methods.$add(self.model, 'foes', CampaignFoe, foe)
       self.locals.active_npc = self.model.foes[self.model.foes.length - 1].id
     }
 
-    self.methods.moveNPC = (npc: CampaignNpc, type: string): void => {
-      if (type === 'foes') {
-        self.methods.addFoe(npc, npc.name)
+    self.methods.moveNPC = (npc: CampaignNpc | CampaignFoe): void => {
+      if (npc instanceof CampaignNpc) {
+        self.methods.addFoe(npc)
         self.methods.removeByObject(self.model.npcs, npc)
       } else {
-        self.methods.addNPC(npc, npc.name)
+        self.methods.addNPC(npc)
         self.methods.removeByObject(self.model.foes, npc)
       }
     }
 
-    self.methods.cloneNPC = (npc: CampaignNpc, type: string): void => {
-      if (type === 'foes') {
-        self.methods.addFoe(npc, npc.name)
+    self.methods.cloneNPC = (npc: CampaignNpc | CampaignFoe): void => {
+      const cloneNPC = { ...npc };
+      cloneNPC.name = `Copy of ${npc.name}`
+      if (npc instanceof CampaignFoe) {
+        self.methods.addFoe(cloneNPC)
       } else {
-        self.methods.addNPC(npc, npc.name)
+        self.methods.addNPC(cloneNPC)
       }
     }
 
